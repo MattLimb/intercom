c944e28c3aa5b4e53afa4dc0220ea4ad6cd5b31f
Matt Limb <matt.limb17@gmail.com>
2020-11-20 19:40:55 +0000

    removing test file

--- test.py
+++
@@ -1,9 +0,0 @@
-import gitlab
-
-gi = gitlab.Gitlab("https://gitlab.com/")
-
-print(gi.projects.get(10582521).name)
-print(gi.projects.get(10582521).web_url)
-
-
-print(gi.projects.get(10582521).releases.list(sort="desc")[0].tag_name)
